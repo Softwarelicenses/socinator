025275b780d22ba5cd35e26df7f888588f807844
Softwarelicenses <59008636+Softwarelicenses@users.noreply.github.com>
2021-01-07 06:08:36 +0100

    Add files via upload

---
+++ check-license.php
@@ -0,0 +1,1 @@
+{ "code" : "ok", "email" : "admin@nullnoss.org", "invoice_id" : "000", "invoice_item_id" : "000", "license_expires" : "2030-01-01 00:00:00", "message" : "OK", "name" : "nullnoss.org", "next_check" : 438265, "scheme_id" : "3", "scheme_title" : "Custom Plan", "username" : "nullnoss.org" }
